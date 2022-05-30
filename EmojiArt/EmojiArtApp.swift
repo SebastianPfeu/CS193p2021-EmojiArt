86941d10c8e6c2c9b67fff8510ddf6a962943ddc
Sebastian Pfeufer <sebastianpfeufer@icloud.com>
2022-05-30 17:59:15 +0200

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Sebastian Pfeufer on 30.05.22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
